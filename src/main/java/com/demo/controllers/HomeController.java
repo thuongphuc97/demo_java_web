aa9c0a7a618566bd148f481284e0f7edfb90be83
thuongphuc97 <thuongphuc97@gmail.com>
2019-12-09 02:24:50 +0700

    second commit

---
+++ src/main/java/com/demo/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+
+	public String sayHello(ModelMap map) {
+		map.addAttribute("msg","Thuong quang phuc");
+		return "hello";
+	}
+
+}
