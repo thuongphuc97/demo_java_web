bf3c9b57e41aebf4bbf7b92e3642df6c09fc2c8e
pthuong <pthuong@dxc.com>
2019-12-13 11:40:54 +0700

    add hibernate config

--- src/main/java/com/demo/configuration/AppConfiguration.java
+++ src/main/java/com/demo/configuration/AppConfiguration.java
@@ -1,15 +1,20 @@
 package com.demo.configuration;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-
-import org.springframework.context.MessageSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -20,7 +25,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @Configuration
 @ComponentScan(basePackages = "com.demo")
-public class AppConfiguration implements WebMvcConfigurer   {
+public class AppConfiguration implements WebMvcConfigurer {
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -29,22 +34,25 @@ public class AppConfiguration implements WebMvcConfigurer   {
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
+
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resource/**").addResourceLocations("/resources/");
 	}
+
 	@Bean
 	public MessageSource messageSource() {
-		 ReloadableResourceBundleMessageSource bundleMessageSource  = new ReloadableResourceBundleMessageSource();
-		 bundleMessageSource.setBasename("classpath:messages");
-		 bundleMessageSource.setDefaultEncoding("utf-8");
-		 return bundleMessageSource;
+		ReloadableResourceBundleMessageSource bundleMessageSource = new ReloadableResourceBundleMessageSource();
+		bundleMessageSource.setBasename("classpath:messages");
+		bundleMessageSource.setDefaultEncoding("utf-8");
+		return bundleMessageSource;
 	}
+
 	@Bean
 	public DataSource dataSource() {
-		
-		String url="jdbc:mysql://localhost:3306/testdb";
-		String username="root";
-		String password="12345678";
+
+		String url = "jdbc:mysql://localhost:3306/testdb";
+		String username = "root";
+		String password = "12345678";
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl(url);
@@ -52,8 +60,33 @@ public class AppConfiguration implements WebMvcConfigurer   {
 		dataSource.setPassword(password);
 		return dataSource;
 	}
+
 	@Bean
 	public JdbcTemplate jdbcTemplate() {
 		return new JdbcTemplate(dataSource());
 	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactoryBean() {
+		LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+		sessionFactoryBean.setDataSource(dataSource());
+		sessionFactoryBean.setPackagesToScan("com.demo.entity");
+//		=========================
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+		properties.put("hibernate.show_sql", "true");
+//		=========================
+		sessionFactoryBean.setHibernateProperties(properties);
+
+		return sessionFactoryBean;
+	}
+
+	@Bean("transactionManager")
+	@Autowired
+	public HibernateTransactionManager hibernateTransactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+		hibernateTransactionManager.setSessionFactory(sessionFactory);
+		return hibernateTransactionManager;
+	}
+
 }
--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,23 +1,15 @@
 package com.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.demo.model.User;;
+import org.springframework.web.bind.annotation.RequestMethod;;
 
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String sayHello(ModelMap map, @RequestParam(name = "uid", required = false) String userid) {
-		map.addAttribute("msg", userid);
+	public String sayHello(ModelMap map) {
+		map.addAttribute("msg", "Xin chao");
 		return "hello";
 	}
 }
